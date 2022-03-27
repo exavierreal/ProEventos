ee9685ef5fa1923ace39e03a79cfb0c93e7aed7b
Ericson Xavier dos Santos <ericson.santos@iatec.com>
2022-03-26 22:39:06 -0300

    First commit

---
+++ server/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .Net 5",
+                Local = "Curitiba",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "Foto1.png"
+            },
+            new Evento() {
+                EventoId = 2,
+                Tema = "JS e Node",
+                Local = "Ponta Grossa",
+                Lote = "1º Lote",
+                QtdPessoas = 30,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "jsenode.png"
+            }
+        };
+
+        public EventoController()
+        {       
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}
